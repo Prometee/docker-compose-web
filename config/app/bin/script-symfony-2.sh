ef6ccb687a49d12619a8659e941835b0637047c2
Francis Hilaire <francis.hilaire@gmail.com>
2018-04-06 00:09:08 +0200

    script chmod wrong

--- config/app/bin/script-symfony-2.sh
+++ config/app/bin/script-symfony-2.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-[ ! -d ${APP_CACHE_DIR} ] && mkdir -p ${APP_CACHE_DIR}
-[ ! -d ${APP_LOGS_DIR} ] && mkdir -p ${APP_LOGS_DIR}
-[ ! -d ${APP_SESSIONS_DIR} ] && mkdir -p ${APP_SESSIONS_DIR}
+[ ! -d ${APP_ROOT}/${APP_CACHE_DIR} ] && mkdir -p ${APP_ROOT}/${APP_CACHE_DIR}
+[ ! -d ${APP_ROOT}/${APP_LOGS_DIR} ] && mkdir -p ${APP_ROOT}/${APP_LOGS_DIR}
+[ ! -d ${APP_ROOT}/${APP_SESSIONS_DIR} ] && mkdir -p ${APP_ROOT}/${APP_SESSIONS_DIR}
 
 chmod 777 \
-    ${APP_CACHE_DIR}/ \
-    ${APP_LOGS_DIR}/ \
-    ${APP_SESSIONS_DIR}/
+    ${APP_ROOT}/${APP_CACHE_DIR} \
+    ${APP_ROOT}/${APP_LOGS_DIR} \
+    ${APP_ROOT}/${APP_SESSIONS_DIR}/
 
 exec "$@"
