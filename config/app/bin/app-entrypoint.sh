25b055f36640bc1175997c12ecf8cbc05e169f43
Francis Hilaire <Prometee@users.noreply.github.com>
2018-08-10 12:09:06 +0200

    Delete custom bind mount for Symfony, this can cause many problems while creating and using a project

--- config/app/bin/app-entrypoint.sh
+++ config/app/bin/app-entrypoint.sh
@@ -8,4 +8,10 @@ then
 fi
 cp -f /usr/local/etc/php/conf.project_type.d/*${APP_PROJECT_TYPE}.ini /usr/local/etc/php/conf.custom.d/
 
+APP_ENTRYPOINT_PROJECT_TYPE="$(dirname $0)/script-${APP_PROJECT_TYPE}.sh"
+if [ -x "${APP_ENTRYPOINT_PROJECT_TYPE}" ]
+then
+    ${APP_ENTRYPOINT_PROJECT_TYPE}
+fi
+
 exec "$@"
